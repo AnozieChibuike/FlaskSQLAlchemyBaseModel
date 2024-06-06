f1de25d79a17dfc40ca72180b914dcf574bd598c
AnozieChibuike <chibuikeanozie0@gmail.com>
2024-06-06 01:48:37 +0100

    v1.1.3 added more methods for the basemodel

--- flasksqlalchemybasemodel/src.py
+++ flasksqlalchemybasemodel/src.py
@@ -3,9 +3,11 @@ import uuid
 from flask import abort
 from sqlalchemy.orm import Mapped
 from flask_sqlalchemy import SQLAlchemy
-from typing import Any
+from typing import NoReturn, Type, TypeVar
 
 db = SQLAlchemy()
+
+T = TypeVar("T", bound="BaseModel")
 
 
 class BaseModel(db.Model):  # type: ignore[name-defined]
@@ -48,6 +50,18 @@ class BaseModel(db.Model):  # type: ignore[name-defined]
     def close(self) -> None:
         db.session.remove()
 
+    def update(self, updates: dict) -> None:
+        """
+        Takes a dictionary of updates and sets the corresponding attribute
+        Keyword arguments:
+        updates -- A dictionary of the Model attributes you wish to update
+        Return: None
+        """
+        for key, value in updates.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.save()
+    
     def to_dict(self) -> dict:
         """
         Returns a dictionary representation of the object.
@@ -73,14 +87,14 @@ class BaseModel(db.Model):  # type: ignore[name-defined]
         return attributes
 
     @classmethod
-    def all(cls):
+    def all(cls: Type[T]) -> list[T | None]:
         """
         Retrieves all objects of the current model
         """
         return cls.query.all()
 
     @classmethod
-    def get(cls, id: str):
+    def get(cls: Type[T], id: str) -> T | None:
         """
         Retrieve an object by its id or name or email or username.
         Returns the object if found, None otherwise.
@@ -92,15 +106,98 @@ class BaseModel(db.Model):  # type: ignore[name-defined]
         return None
 
     @classmethod
-    def get_or_404(cls, id: str):
+    def get_or_404(cls: Type[T], id: str) -> T | NoReturn:
         """
         Retrieve an object by its id
-        Returns the object if found, None otherwise.
+        Returns the object if found, Throws a flask.abort(404) otherwise
         Usage:
-            cls.get(id=<id>)
+            cls.get_or_404(id=<id>)
         """
-        if id:
-            return cls.query.get(id) or abort(
-                404, description="Item with given ID not found"
-            )
-        return None
+        return cls.query.get(id) or abort(
+            404, description="Item with given ID not found"
+        )
+
+    @classmethod
+    def filter_one(cls: Type[T], **kwargs) -> T | None:
+        """
+        Filter the Object by the parameters supplied
+        Arguments:
+            **kwargs:
+        Return:
+            A single object if found, None otherwise
+        """
+        return cls.query.filter_by(**kwargs).first()
+
+    @classmethod
+    def filter_all(cls: Type[T], **kwargs) -> list[T | None]:
+        """
+        Filter the Object by the parameters supplied
+
+        Arguments:
+            **kwargs:
+        Return:
+            List of objects matching the parameters supplied, None otherwise
+        """
+        return cls.query.filter_by(**kwargs).all()
+
+    @classmethod
+    def filter_and_count_all(cls: Type[T], **kwargs) -> int:
+        """
+        Get the count of objects by the filtered parameter
+
+        Arguments:
+            **kwargs:
+        Return:
+            The count of objects found by query
+        """
+        return cls.query.filter_by(**kwargs).count()
+
+    @classmethod
+    def count_all(cls: Type[T]) -> int:
+        """
+        Get the count of items
+
+        Arguments:
+            **kwargs:
+        Return:
+            The count of objects
+        """
+        return cls.query.all().count()
+
+    @classmethod
+    def exists(cls: Type[T], **kwargs) -> bool:
+        """
+        Check if an item exists
+
+        Arguments:
+            **kwargs:
+        Return:
+            True if exists and False otherwise
+        """
+        return cls.filter_one(**kwargs) is not None
+
+    @classmethod
+    def paginate(cls: Type[T], page: int = 1, per_page: int = 10) -> dict:
+        """
+        Paginate items in a model
+
+        Arguments:
+            **kwargs:
+        Return:
+            Dictionary of the pagination:\n
+            {
+                "items": pagination.items,
+                "total": pagination.total,
+                "pages": pagination.pages,
+                "page": pagination.page,
+                "per_page": pagination.per_page\n
+            }
+        """
+        pagination = cls.query.paginate(page=page, per_page=per_page, error_out=False)
+        return {
+            "items": pagination.items,
+            "total": pagination.total,
+            "pages": pagination.pages,
+            "page": pagination.page,
+            "per_page": pagination.per_page,
+        }
